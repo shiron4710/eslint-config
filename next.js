2093ade09af4409945ebfb122be0edffc3878942
shiron <shiron4710.dev@gmail.com>
2023-05-06 01:33:15 +0900

    fix: quotes rule in next

--- next.js
+++ next.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: [require.resolve("./index.js"), "plugin:@next/next/core-web-vitals"]
+  extends: [require.resolve("./index.js"), "plugin:@next/next/core-web-vitals"],
+  rules: {
+    "@typescript-eslint/quotes": ["error", "double"]
+  }
 }
